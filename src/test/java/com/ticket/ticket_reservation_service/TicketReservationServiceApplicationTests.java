3e4d0560a0e8de87927b8e846293c26236e390a3
saikaldi <saikalulukbek24@gmail.com>
2026-08-11 13:11:00 +0600

    Initial commit: infrastructure setup and flyway migration

---
+++ src/test/java/com/ticket/ticket_reservation_service/TicketReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket.ticket_reservation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
